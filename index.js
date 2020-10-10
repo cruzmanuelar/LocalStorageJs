8fdefd0873c949f93e4a3c2c26b1fef003cb9c1a
Manuel Cruz <cruzmanuelar@gmail.com>
2020-10-10 18:18:19 -0500

    EliminarDeLocalStorage

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ function guardarTarea(e) {
       descripcion
     };
   
-    //console.log(tarea)
-  
     if(localStorage.getItem('tareas') === null) {
       let tareas = [];
       tareas.push(tarea);
@@ -25,6 +23,20 @@ function guardarTarea(e) {
     e.preventDefault();
 }
 
+function eliminarTarea(titulo) {
+
+    let tareas = JSON.parse(localStorage.getItem('tareas'));
+    for(let i = 0; i < tareas.length; i++) {
+      if(tareas[i].titulo == titulo) {
+        tareas.splice(i, 1);
+      }
+    }
+  
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+    mostrarTareas();
+}
+
+
 function mostrarTareas() {
     let tareas = JSON.parse(localStorage.getItem('tareas'));
     let vistaTareas = document.getElementById('tareas');
@@ -34,15 +46,31 @@ function mostrarTareas() {
       let descripcion = tareas[i].descripcion;
   
       vistaTareas.innerHTML += `
-        <div class="row">
+        <div class="row my-2 px-4">
             <div class="col-6">
-                <p class="text-dark">${titulo} - ${descripcion}
+                <p class="text-dark">${titulo} : ${descripcion}
             </div>
-            <div class="col-6">
-                <a href="#" class="btn btn-danger ml-5">Eliminar</a>
+            <div class="col-6 text-right">
+                <a href="#" onclick="eliminarTarea('${titulo}')" class="btn btn-info">Hecho</a>
             </div>
         </div>`;
     }
+}
+
+function limpiarTareas(e){
+
+    let tareas = JSON.parse(localStorage.getItem('tareas'));
+    if(tareas===null){
+        alert("Registro vacío");
+    }else{
+        let eliminar = prompt("¿Eliminar todo el registro? S/N");
+        if(eliminar==="S" || eliminar ==="s"){
+            localStorage.clear();
+        }
+    }
+    document.getElementById('formTarea').reset();
+    mostrarTareas();
+
 }
 
 mostrarTareas();
