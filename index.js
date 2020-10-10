0f52c5cadb955a4f6667e3ffe6731ad40414a2ad
Manuel Cruz <cruzmanuelar@gmail.com>
2020-10-10 17:11:55 -0500

    MostrarTareas

--- index.js
+++ index.js
@@ -20,5 +20,29 @@ function guardarTarea(e) {
       tareas.push(tarea);
       localStorage.setItem('tareas', JSON.stringify(tareas));
     }
-  
+    mostrarTareas();
+    document.getElementById('formTarea').reset();
+    e.preventDefault();
 }
+
+function mostrarTareas() {
+    let tareas = JSON.parse(localStorage.getItem('tareas'));
+    let vistaTareas = document.getElementById('tareas');
+    vistaTareas.innerHTML = '';
+    for(let i = 0; i < tareas.length; i++) {
+      let titulo = tareas[i].titulo;
+      let descripcion = tareas[i].descripcion;
+  
+      vistaTareas.innerHTML += `
+        <div class="row">
+            <div class="col-6">
+                <p class="text-dark">${titulo} - ${descripcion}
+            </div>
+            <div class="col-6">
+                <a href="#" class="btn btn-danger ml-5">Eliminar</a>
+            </div>
+        </div>`;
+    }
+}
+
+mostrarTareas();
